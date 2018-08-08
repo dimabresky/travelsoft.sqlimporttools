b1c4d3b92355a5dbf025e9f7be092e524fd5c563
dimabresky <dimabresky@192.168.100.6>
2018-08-08 18:39:56 +0300

    Exporter class is added

--- include.php
+++ include.php
@@ -4,7 +4,8 @@ $classes = array(
     
     "\\travelsoft\\sqlparsertools\\Config" => "lib/Config.php",
     "\\travelsoft\\sqlparsertools\\Files" => "lib/Files.php",
-    
+    "\\travelsoft\\sqlparsertools\\export\\Exporter" => "lib/export/Exporter.php",
+    "\\travelsoft\\sqlparsertools\\export\\Exporter" => "lib/export/Exporter.php",
 );
 
 
---
+++ lib/export/Exporter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace travelsoft\sqlparsertools\export;
+
+/**
+ * Абстрактный класс экспорта
+ *
+ * @author dimabresky
+ */
+abstract class Exporter {
+    abstract public function export();
+}
