67f07241f1c8a8aa4fb355a373365a4512a9d889
dimabresky <dimabresky@192.168.100.6>
2018-08-08 18:45:50 +0300

    include export classes are added

--- include.php
+++ include.php
@@ -5,7 +5,11 @@ $classes = array(
     "\\travelsoft\\sqlparsertools\\Config" => "lib/Config.php",
     "\\travelsoft\\sqlparsertools\\Files" => "lib/Files.php",
     "\\travelsoft\\sqlparsertools\\export\\Exporter" => "lib/export/Exporter.php",
-    "\\travelsoft\\sqlparsertools\\export\\Exporter" => "lib/export/Exporter.php",
+    "\\travelsoft\\sqlparsertools\\export\\Cities" => "lib/export/Cities.php",
+    "\\travelsoft\\sqlparsertools\\export\\Hotels" => "lib/export/Hotels.php",
+    "\\travelsoft\\sqlparsertools\\export\\Facilities" => "lib/export/Facilities.php",
+    "\\travelsoft\\sqlparsertools\\export\\Policies" => "lib/export/Policies.php",
+    "\\travelsoft\\sqlparsertools\\export\\Rooms" => "lib/export/Rooms.php",
 );
 
 
